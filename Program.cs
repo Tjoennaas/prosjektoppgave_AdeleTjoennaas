b0ec0323da4e9b32c6ee84be79e6a4e000d063e9
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2026-03-23 12:17:08 +0100

    Lagt til serilog for sporing og feilhåndtering

--- Program.cs
+++ Program.cs
@@ -1,19 +1,27 @@
 
+
+using Serilog;
 using ProsjektOppgave_AdeleTjoennaas.Services;
 using ProsjektOppgave_AdeleTjoennaas.BackgroundTask;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
+//Ref serielog: https://www.nuget.org/packages/Serilog.AspNetCore
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("log.txt")
+     .CreateLogger();
+
+
+     builder.Services.AddSerilog();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient<AzurePriceService>();
-builder.Services.AddHostedService<AzureBackgroundService >();
+builder.Services.AddHostedService<AzureBackgroundService>();
 
 
 var app = builder.Build();
 
-
-
 app.Run();
-
