ace3361fd456c129b11ab1256c19cb25eb935a6f
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2026-03-25 16:20:06 +0100

    Lagt til migratin for sqlite

--- Data/Context.cs
+++ Data/Context.cs
@@ -1,5 +1,20 @@
 
 
+using Microsoft.EntityFrameworkCore;
+using ProsjektOppgave_AdeleTjoennaas.Models;
 
+
+namespace ProsjektOppgave_AdeleTjoennaas.Data{
+
+
+
+public class PriceAzureContext : DbContext {  
+
+
+ public PriceAzureContext(DbContextOptions<PriceAzureContext> options)
+              : base(options) { }
+
+     public DbSet<AzurePrice> AzurePrices { get; set; }
+}}
     
 
---
+++ Migrations/20260325145110_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace prosjektoppgave_AdeleTjoennaas.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AzurePrices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ArmRegionName = table.Column<string>(type: "TEXT", nullable: false),
+                    ProductName = table.Column<string>(type: "TEXT", nullable: false),
+                    RetailPrice = table.Column<decimal>(type: "TEXT", nullable: false),
+                    CurrencyCode = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AzurePrices", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AzurePrices");
+        }
+    }
+}
