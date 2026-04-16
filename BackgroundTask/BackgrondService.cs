9ccdd45f4979f2f7426cb5097e64b84de27cb541
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2026-04-16 16:41:17 +0200

    Begrense antall objekter per side

--- BackgroundTask/BackgrondService.cs
+++ BackgroundTask/BackgrondService.cs
@@ -1,8 +1,6 @@
 
 
 
-// Kilde/Ref: https://learn.microsoft.com/en-us/aspnet/core/fundamentals/host/hosted-services?view=aspnetcore-10.0&tabs=visual-studio#asynchronous-timed-background-task
-
 
 
 using ProsjektOppgave_AdeleTjoennaas.Services;
@@ -43,7 +41,7 @@ namespace ProsjektOppgave_AdeleTjoennaas.BackgroundTask {
 
             if (hasData && !isStale)
             {
-                _logger.LogInformation("Data finnes allerede og er fortsatt gyldige.");
+                _logger.LogInformation("Data finnes allerede og er gyldige.");
                 return;
             }
 
@@ -78,87 +76,6 @@ namespace ProsjektOppgave_AdeleTjoennaas.BackgroundTask {
 
 
 
-
-
-/*
-
-public class AzureBackgroundService : BackgroundService {
-       private readonly ILogger<AzureBackgroundService> _logger;
-       // private object dbContext;
-
-        public AzureBackgroundService(IServiceProvider services, 
-        ILogger<AzureBackgroundService> logger) {  
-
-        Services = services;
-        _logger = logger;
- }
-    public IServiceProvider Services { get; }
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        _logger.LogInformation("Service starter.");
-
-        await DoWork(stoppingToken);
-
-        using PeriodicTimer timer = new(TimeSpan.FromHours(24));
-
-        try
-        {
-            while (await timer.WaitForNextTickAsync(stoppingToken))
-            {
-                await DoWork(stoppingToken);
-            }
-        }
-        catch (OperationCanceledException)
-        {
-            _logger.LogInformation("Service stoppet.");
-        }}
-        
-
-   //----------------------------------------------------------------// 
-
-
-    private async Task DoWork(CancellationToken stoppingToken){
-    try {
-         _logger.LogInformation("Starter henting av informasjon fra Azure");
-
-    using (var scope = Services.CreateScope())
-        {
-            var price = scope.ServiceProvider.GetRequiredService<AzurePriceService>();         
-            var dbContext = scope.ServiceProvider.GetRequiredService<PriceAzureContext>();
-
-
-
- var currencies = new []{"USD", "EUR", "GBP"};
-var regions = new[] { "westeurope", "northeurope" };
- var allPrices = new List<AzurePrice>();
-
-
-   foreach (var region in regions)
-        {
-            foreach (var currency in currencies)
-            {
-                var prices = await price.GetPricesAsync( region,currency);
-                allPrices.AddRange(prices);
-            }
-        }
-
-    
-        dbContext.AzurePrices.RemoveRange(dbContext.AzurePrices);
-        await dbContext.SaveChangesAsync(stoppingToken);
-
-        await dbContext.AzurePrices.AddRangeAsync(allPrices, stoppingToken);
-        await dbContext.SaveChangesAsync(stoppingToken);}}
-
-catch (Exception ex)
-            {
-                _logger.LogError(ex, "Feil ved henting av Azure-priser.");
-            
-            }}}};
-
-  */
-
-
-        
 
        
 
