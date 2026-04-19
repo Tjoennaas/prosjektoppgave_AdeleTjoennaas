fd584e652b4dbdca5f10a431e6cdb742d76da87d
Adele Tjønnås <145026954+Tjoennaas@users.noreply.github.com>
2026-04-19 15:10:29 +0200

    Refactor CustomerInput class to include new properties

--- Models/CustomerInput.cs
+++ Models/CustomerInput.cs
@@ -1,30 +1,18 @@
 
-using System.ComponentModel.DataAnnotations;
 
 
 namespace ProsjektOppgave_AdeleTjoennaas.Models {
-public class AzurePrice
+
+public class  CustomerInput
 {
-        [Key]
-        public int Id { get; set; }
-        public string? CurrencyCode { get; set; }
-        public decimal TierMinimumUnits { get; set; }
-        public decimal RetailPrice { get; set; }
-        public decimal UnitPrice { get; set; }
-        public string? ArmRegionName { get; set; }
-        public string? Location { get; set; } 
-        public DateTime? EffectiveStartDate { get; set; }
-        public string? MeterId { get; set; }
-        public string? MeterName { get; set; }
-        public string? ProductId { get; set; }
-        public string? SkuId { get; set; }
-        public string? ProductName { get; set; }
-        public string? SkuName { get; set; }
-        public string? ServiceName { get; set; }
-        public string? ServiceId { get; set; }
-        public string? ServiceFamily { get; set; }
-        public string? Type { get; set; }
-        public bool? IsPrimaryMeterRegion { get; set; }
-        public string? ArmSkuName { get; set; }
-        public DateTime LastUpdatedUtc { get; internal set; }
+
+   public int ActiveUsers { get; set; }
+
+    public int EventsPerPeriod { get; set; }
+
+    public int RetentionPeriods { get; set; }
+
+    public int AdditionalEventCollectors { get; set; }
+
+  
 }}
