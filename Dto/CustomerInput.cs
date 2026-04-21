5951fa7291ec51cb485f6b7d468a92ae01358301
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2026-04-21 20:42:04 +0200

    Lagt til anotering

--- Dto/CustomerInput.cs
+++ Dto/CustomerInput.cs
@@ -1,15 +1,18 @@
 
-
+using System.ComponentModel.DataAnnotations;
 
 namespace ProsjektOppgave_AdeleTjoennaas.Dto {
 
 public record CustomerInput
 (
-
+ 
+   [Range(1, int.MaxValue)]
    int ActiveUsers,
-
+ 
+   [Range(1, int.MaxValue)]
    int EventsPerPeriod,
-
+  
+   [Range(1, int.MaxValue)]
    int RetentionPeriods, 
 
    int? Collector
