ebfdccdd5e8ebdf9da1ab5cc82250ad453295449
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2026-04-24 15:21:45 +0200

    Rettelse i koden

--- Dto/CustomerInput.cs
+++ Dto/CustomerInput.cs
@@ -5,19 +5,13 @@ namespace ProsjektOppgave_AdeleTjoennaas.Dto {
 
 public record CustomerInput
 (
- 
-   [Range(1, int.MaxValue)]
+[Range(1, int.MaxValue)]
    int ActiveUsers,
- 
-   [Range(1, int.MaxValue)]
+[Range(1, int.MaxValue)]
    int EventsPerPeriod,
-  
-   [Range(1, int.MaxValue)]
+[Range(1, int.MaxValue)]
    int RetentionPeriods, 
-
+[Range(1, int.MaxValue)]
    int? Collector
 
-  
-
-  
 );}
