40e8fe5160605c99b29cf996aea70bb25cdc79c7
Tjoennaas <145026954+Tjoennaas@users.noreply.github.com>
2026-05-03 10:20:20 +0200

    Ny migrering

--- Migrations/20260503075125_AddPeriodNumberToCalculationMargin.Designer.cs
+++ Migrations/20260503075125_AddPeriodNumberToCalculationMargin.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProsjektOppgave_AdeleTjoennaas.Data;
 
@@ -10,9 +11,11 @@ using ProsjektOppgave_AdeleTjoennaas.Data;
 namespace prosjektoppgave_AdeleTjoennaas.Migrations
 {
     [DbContext(typeof(PriceDbContext))]
-    partial class PriceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260503075125_AddPeriodNumberToCalculationMargin")]
+    partial class AddPeriodNumberToCalculationMargin
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.11");
